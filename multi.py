4d400674c197caef2a8853a943cb2f3716bba5fd
NickMao05 <69585388+NickMao05@users.noreply.github.com>
2023-01-27 03:39:43 +0800

    Update multi.py

--- multi.py
+++ multi.py
@@ -8,6 +8,9 @@ logger = None
 loop = True
 job = True
 
+os.environ['REQS_FILE'] = 'requirements.txt'
+os.environ['COMMANDLINE_ARGS'] = '--no-half-vae --port 33897 --ngrok 2K95gmylUQ7mztGzNeIPK6h1oI0_3AZs2je3o12Ai8d2qj1PU --enable-insecure-extension-access --localizations-dir /kaggle/working/stable-diffusion-webui/localizations/  --deepdanbooru --disable-safe-unpickle --xformers '
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 # 保存进程ID
 def savepid(pid):
     with open(pidfile, 'w') as f:
@@ -108,7 +111,7 @@ def logrotate():
 
 def main():
     # 业务逻辑
-    print("hello nick")
+    exec(open("launch.py").read())
     # 业务逻辑
     time.sleep(2)
 
