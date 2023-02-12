2e3d2df7db520e0a6107ab1ab56f388ae780fde7
Nick Mao <8824898+nick-mao@user.noreply.gitee.com>
2023-02-12 03:25:49 +0000

    删除文件 stable-diffusion-webui/extensions/sd-webui-tunnels-main

--- stable-diffusion-webui/extensions/sd-webui-tunnels-main/scripts/try_cloudflare.py
+++
@@ -1,8 +0,0 @@
-from pycloudflared import try_cloudflare
-
-from modules.shared import cmd_opts
-
-if cmd_opts.cloudflared:
-    print("cloudflared detected, trying to connect...")
-    port = cmd_opts.port if cmd_opts.port else 7860
-    try_cloudflare(port)
